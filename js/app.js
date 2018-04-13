d5779950fc1c8aff71f4196375d5b7aa0942cef9
mapetric <32669888+mapetric@users.noreply.github.com>
2018-04-13 20:19:46 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,30 +1,10 @@
 let inputJSON, outputJSON = null;
 
-// poruke za popup poredane kako se pojavljuju stupci
-
-const popupMessages = ['ime kandidata', 'prezime kandidata', 'spol kandidata', 'dob kandidata', 'datum prijave kandidata'];
-
-// aray sa svim input poljima
-
 const arr = $('input');
-
-// za pomak popup-a da izbjegnemo vizualno titranje
-
-const moveLeft = 20;
-const moveDown = 10;
-
-// mijenja format datuma i trimira 0 ako se nalazi na prvom mjestu kod dana i mjeseca
-
-const modifyDate = (date) => {
-      let date1 = date.split('-');
-      date1[1] = date1[1][0] === '0' ? date1[1][1] : date1[1];
-      date1[2] = date1[2][0] === '0' ? date1[2][1] : date1[2];
-      return `${date1.reverse().join('.')}.`;
-};
 
 // mijenja mjesto dana i mjeseca
 
-const changeDateFormat = (date) => {
+const formatDate = (date) => {
       let date1 = date.split('.');
       let date2 = [];
       date2.push(date1[1]);
@@ -39,7 +19,7 @@ const changeDateFormat = (date) => {
 
 const howOld = (dateOfBirth) => {
       const today = new Date();
-      const birthDate = new Date(changeDateFormat(dateOfBirth));
+      const birthDate = new Date(formatDate(dateOfBirth));
       return today.getFullYear() - birthDate.getFullYear();
 };
 
@@ -48,7 +28,7 @@ const howOld = (dateOfBirth) => {
 const fillTable = () => {
       inputJSON.registrations.forEach(function(element){
             $('#table').append(
-                  `<tr>
+                  `<tr title="Datum rođenja: ${element.date_of_birth}">
                         <td>${element.name}</td>
                         <td>${element.surname}</td>
                         <td>${element.sex === "male" ? "M" : "F"}</td>
@@ -67,20 +47,18 @@ const addToTable = () => {
                   {"name": $('#name').val(),
                   "surname": $('#surname').val(),
                   "sex": $('#gender').val(),
-                  "date_of_birth": modifyDate($('#dateOfBirth').val()),
-                  "date_of_registration": modifyDate($('#dateOfRegistration').val())});
+                  "date_of_birth": $('#dateOfBirth').val(),
+                  "date_of_registration": $('#dateOfRegistration').val()
+            });
             $('#table').append(
-                  `<tr>
-                        <td>${$('#name').val()}</td>
-                        <td>${$('#surname').val()}</td>
-                        <td>${$('#gender').val() === "male" ? "M" : "F"}</td>
-                        <td>${howOld($('#dateOfBirth').val())}</td>
-                        <td>${modifyDate($('#dateOfRegistration').val())}</td>
+                  `<tr title="Datum rođenja: ${outputJSON.registrations[outputJSON.registrations.length - 1].date_of_birth}">
+                        <td>${outputJSON.registrations[outputJSON.registrations.length - 1].name}</td>
+                        <td>${outputJSON.registrations[outputJSON.registrations.length - 1].surname}</td>
+                        <td>${outputJSON.registrations[outputJSON.registrations.length - 1].sex === "male" ? "M" : "F"}</td>
+                        <td>${howOld(outputJSON.registrations[outputJSON.registrations.length - 1].date_of_birth)}</td>
+                        <td>${outputJSON.registrations[outputJSON.registrations.length - 1].date_of_registration}</td>
                   </tr>`);
 
-// event listener koji pokazuje datum rodena kad smo na redu i skriva ga kad uklonimo mis sa reda
-
-            $('tr').last().mouseenter(displayDateOfBirth).mouseleave(hideDateOfBirth);
       } else {
             handleMissing();
       }
@@ -106,24 +84,11 @@ const removeMissing = () => {
       }
 }
 
-
 const sendToPastebin = () => {
 
       // napravi nekaj sa outputJSON
 
 };
-
-// nije fat arrow funkcija jer one bindaju this na window object, dodaje td sa datumom rodenja odredenog reda
-
-function displayDateOfBirth() {
-      $(`#table tr:nth-child(${$(this).index() + 1})`).append(`<td>Datum rođenja: ${outputJSON.registrations[$(this).index()-1].date_of_birth}</td>`);
-}
-
-// nije fat arrow funkcija jer one bindaju this na window object, uklanja td sa datumom rodenja odredenog reda
-
-function hideDateOfBirth() {
-      $(`#table tr:nth-child(${$(this).index() + 1})`).children().last().remove();
-}
 
 // ucita inputJSON sa lokalnog servera
 
@@ -134,26 +99,14 @@ $.getJSON('data.json').then(function(response) {
       console.error("Failed!", error);
 }).then(function() {
 
-// popunjava tablicu, dodaje event listenere za gumbe, redke (ako nisu header) za prikaz datuma rodenja, popup za header red
+// popunjava tablicu, dodaje event listenere za gumbe, i namjesta datepicker za datume
 
       fillTable();
       $('#addTableRow').click(addToTable);
       $('#sendJSON').click(sendToPastebin);
-      $('tr').not($('#header')).mouseenter(displayDateOfBirth).mouseleave(hideDateOfBirth);
-      $('#header td').hover(function(e) {
+      $( "#dateOfBirth" ).datepicker({ dateFormat: 'dd.mm.yy.' });
+      $( "#dateOfRegistration" ).datepicker({ dateFormat: 'dd.mm.yy.' });
 
-// mijenja sadrzaj popup-a ovisno o indexu td u kojem smo (uzima poruku iz popupMessages arraya)
-
-            $('.pop-up').text(`${popupMessages[$(this).index()]}`).show();
-      }, function() {
-            $('.pop-up').hide();
-      });
-
-// mice div sa popup-om kada se krecemo po header redu
-
-      $('#header').mousemove(function(e) {
-            $(".pop-up").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
-      });
       console.log("Loaded the JSON successfuly!");
 }, function(error) {
       console.error("Failed!", error);
